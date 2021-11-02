3b557f1075ea4f6d16bd3614d626068128ae16c6
Ashutosh Biswas <ashutoshbw314@gmail.com>
2021-11-02 19:15:47 +0600

    Added gitconfig, some installing logic and did some i3 configuration

--- install.sh
+++ install.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-########
-# nvim #
-########
+# =[ X11 ]=
+rm -rf "$HOME/.config/X11"
+ln -s "$HOME/dotfiles/X11" "$HOME/.config"
 
+# =[ i3 ]=
+rm -rf "$HOME/.config/i3"
+ln -s "$HOME/dotfiles/i3" "$HOME/.config"
+
+# =[ alacritty ]=
+rm -rf "$HOME/.config/alacritty"
+ln -s "$HOME/dotfiles/alacritty" "$HOME/.config"
+
+# =[ nvim ]=
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/dotfiles/nvim/init.vim" "$HOME/.config/nvim/init.vim"
 
+# =[ ranger ]=
+rm -rf "$HOME/.config/ranger"
+ln -s "$HOME/dotfiles/ranger" "$HOME/.config"
 
-#######
-# X11 #
-#######
-rm -rf "$HOME/.config/X11"
-ln -s "$HOME/dotfiles/X11" "$HOME/.config"
+# =[ zathura ]=
+rm -rf "$HOME/.config/zathura"
+ln -s "$HOME/dotfiles/zathura" "$HOME/.config"
+
+# =[ gitconfig ]=
+ln -sf "$HOME/dotfiles/gitconfig" "$HOME/.gitconfig"
