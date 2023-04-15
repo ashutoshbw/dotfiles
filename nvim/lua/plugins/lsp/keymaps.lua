d2cb80f152779d9902c1e81c1624754b5f448e20
Ashutosh Biswas <ashutoshbw314@gmail.com>
2023-04-15 07:31:43 +0600

    Enabled range formatting

--- nvim/lua/plugins/lsp/keymaps.lua
+++ nvim/lua/plugins/lsp/keymaps.lua
@@ -32,7 +32,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, opts)
     vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>f", function()
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
       --vim.lsp.buf.format { async = true }
       U.lsp_formatting(ev.buf)
     end, opts)
