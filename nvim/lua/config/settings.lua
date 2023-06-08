fa679cd67562e244f9a23c36a12c45094be8a39d
Ashutosh Biswas <ashutoshbw314@gmail.com>
2023-06-08 19:48:59 +0600

    fixed color issue in tmux

--- nvim/lua/config/settings.lua
+++ nvim/lua/config/settings.lua
@@ -2,28 +2,29 @@ local g = vim.g
 local o = vim.o
 
 -- Map <leader> to space and <localleader> to \
-g.mapleader = ' '
-g.maplocalleader = '\\'
+g.mapleader = " "
+g.maplocalleader = "\\"
 
 -- UI
+o.termguicolors = true
 o.number = true
 o.numberwidth = 5
-o.signcolumn = 'yes:2'
+o.signcolumn = "yes:2"
 o.relativenumber = true
 o.cursorline = true
 o.wrap = true
-o.linebreak = true  -- break line at 'breakat'
+o.linebreak = true -- break line at 'breakat'
 
 -- Editing experience
 o.expandtab = true
 o.tabstop = 2
-o.shiftwidth = 0  -- same as tabstop
+o.shiftwidth = 0 -- same as tabstop
 
 o.list = true
-o.listchars = 'trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂,eol:↴'
+o.listchars = "trail:·,nbsp:◇,tab:→ ,extends:▸,precedes:◂,eol:↴"
 
 -- Makes neovim and host OS clipboard play nicely with each other
-o.clipboard = 'unnamedplus'
+o.clipboard = "unnamedplus"
 
 -- Undo
 o.undofile = true
@@ -31,4 +32,3 @@ o.swapfile = false
 
 -- Remember 50 items in commandline history
 o.history = 50
-
