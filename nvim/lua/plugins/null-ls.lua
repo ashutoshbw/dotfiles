30f11e4da4c597719b4c2ed7f637d530f36cbbf2
Ashutosh Biswas <ashutoshbw314@gmail.com>
2023-11-07 17:04:51 +0600

    Remove markdownlint

--- nvim/lua/plugins/null-ls.lua
+++ nvim/lua/plugins/null-ls.lua
@@ -28,13 +28,11 @@ plugin.config = function()
   local null_ls = require("null-ls")
 
   local formatting = null_ls.builtins.formatting
-  local diagnostics = null_ls.builtins.diagnostics
 
   null_ls.setup({
     sources = {
       formatting.prettierd,
       formatting.stylua,
-      diagnostics.markdownlint,
     },
     on_attach = on_attach,
   })
