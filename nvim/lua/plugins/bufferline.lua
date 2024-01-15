6f55a7bbbce786826efe2bfb0073ea4d972ffe83
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-01-16 01:32:38 +0600

    Setup bufferline for nvim

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,3 +1,10 @@
+local icons = {
+  error = "", -- '✗'
+  warn = "", -- 
+  info = "󰋼", --  ℹ 󰙎 
+  hint = "󰌶", --  ⚑
+}
+
 return {
   "akinsho/bufferline.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
@@ -5,8 +12,22 @@ return {
     local bufferline = require("bufferline")
     bufferline.setup({
       options = {
-        close_icon = ''
+        separator_style = "slant",
+        sort_by = "insert_after_current",
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level)
+          level = level:match("warn") and "warn" or level
+          return (icons[level] or "?") .. " " .. count
+        end,
+        diagnostics_update_in_insert = false,
       },
     })
+
+    vim.keymap.set("n", "[b", "<Cmd>BufferLineMoveNext<CR>", { desc = "bufferline: move next" })
+    vim.keymap.set("n", "]b", "<Cmd>BufferLineMovePrev<CR>", { desc = "bufferline: move prev" })
+    vim.keymap.set("n", "gbb", "<Cmd>BufferLinePick<CR>", { desc = "bufferline: pick buffer" })
+    vim.keymap.set("n", "gbd", "<Cmd>BufferLinePickClose<CR>", { desc = "bufferline: delete buffer" })
+    vim.keymap.set("n", "<S-tab>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "bufferline: prev" })
+    vim.keymap.set("n", "<leader><tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "bufferline: next" })
   end,
 }
