e8c0b454943392f0a6e825a9b10ca2728ff0b761
Ashutosh Biswas <ashutoshbw314@gmail.com>
2024-09-17 21:27:32 +0600

    nvim: remap move plugin commands

--- nvim/lua/plugins/move.lua
+++ nvim/lua/plugins/move.lua
@@ -8,11 +8,11 @@ return {
     local opts = { noremap = true, silent = true }
 
     -- Normal-mode commands
-    vim.keymap.set("n", "<S-d>", ":MoveLine(1)<CR>", opts)
-    vim.keymap.set("n", "<S-u>", ":MoveLine(-1)<CR>", opts)
+    vim.keymap.set("n", "<C-S-j>", ":MoveLine(1)<CR>", opts)
+    vim.keymap.set("n", "<C-S-k>", ":MoveLine(-1)<CR>", opts)
 
     -- Visual-mode commands
-    vim.keymap.set("v", "<S-d>", ":MoveBlock(1)<CR>", opts)
-    vim.keymap.set("v", "<S-u>", ":MoveBlock(-1)<CR>", opts)
+    vim.keymap.set("v", "<C-S-j>", ":MoveBlock(1)<CR>", opts)
+    vim.keymap.set("v", "<C-S-k>", ":MoveBlock(-1)<CR>", opts)
   end,
 }
